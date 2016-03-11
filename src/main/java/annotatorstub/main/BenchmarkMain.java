6047f666189648d03dc2abfd016d49c63ffca0c8
marco <marco@macor.di.unipi.it>
2016-03-11 16:37:57 +0100

    added methods to serialize/deserialize output.

--- src/main/java/annotatorstub/main/BenchmarkMain.java
+++ src/main/java/annotatorstub/main/BenchmarkMain.java
@@ -12,11 +12,13 @@ import it.unipi.di.acube.batframework.problems.A2WDataset;
 import it.unipi.di.acube.batframework.utils.DumpData;
 import it.unipi.di.acube.batframework.utils.WikipediaApiInterface;
 
+import java.io.File;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 
 import annotatorstub.annotator.FakeAnnotator;
+import annotatorstub.utils.Utils;
 
 public class BenchmarkMain {
 	public static void main(String[] args) throws Exception {
@@ -30,23 +32,14 @@ public class BenchmarkMain {
 
 		Metrics<Tag> metricsTag = new Metrics<>();
 		MetricsResultSet C2WRes = metricsTag.getResult(resTag, ds.getC2WGoldStandardList(), new StrongTagMatch(wikiApi));
-		printMetricsResultSet("C2W", C2WRes, ann.getName());
+		Utils.printMetricsResultSet("C2W", C2WRes, ann.getName());
 
 		Metrics<Annotation> metricsAnn = new Metrics<>();
 		MetricsResultSet rsA2W = metricsAnn.getResult(resAnn, ds.getA2WGoldStandardList(), new StrongAnnotationMatch(wikiApi));
-		printMetricsResultSet("A2W-SAM", rsA2W, ann.getName());
+		Utils.printMetricsResultSet("A2W-SAM", rsA2W, ann.getName());
 		
+		Utils.serializeResult(ann, ds, new File("annotations.bin"));
 		wikiApi.flush();
-	}
-
-	private static void printMetricsResultSet(String exp, MetricsResultSet rs, String annName){
-		System.out
-		.format(Locale.ENGLISH,
-				"%s\t mac-P/R/F1: %.3f\t%.3f\t%.3f TP/FP/FN: %d\t%d\t%d mic-P/R/F1: %.3f\t%.3f\t%.3f\t%s%n", exp,
-				rs.getMacroPrecision(), rs.getMacroRecall(),
-				rs.getMacroF1(), rs.getGlobalTp(),
-				rs.getGlobalFp(), rs.getGlobalFn(),rs.getMicroPrecision(), rs.getMicroRecall(),
-				rs.getMicroF1(), annName);
 	}
 
 }
--- src/main/java/annotatorstub/utils/Utils.java
+++ src/main/java/annotatorstub/utils/Utils.java
@@ -16,14 +16,30 @@
 
 package annotatorstub.utils;
 
+import java.io.*;
 import java.lang.invoke.MethodHandles;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import it.unipi.di.acube.batframework.cache.BenchmarkCache;
+import it.unipi.di.acube.batframework.data.Annotation;
+import it.unipi.di.acube.batframework.data.Tag;
+import it.unipi.di.acube.batframework.metrics.Metrics;
+import it.unipi.di.acube.batframework.metrics.MetricsResultSet;
+import it.unipi.di.acube.batframework.metrics.StrongAnnotationMatch;
+import it.unipi.di.acube.batframework.metrics.StrongTagMatch;
+import it.unipi.di.acube.batframework.problems.A2WDataset;
+import it.unipi.di.acube.batframework.problems.A2WSystem;
+import it.unipi.di.acube.batframework.utils.ProblemReduction;
+import it.unipi.di.acube.batframework.utils.WikipediaApiInterface;
 
 public class Utils {
 	private final static Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -50,5 +66,42 @@ public class Utils {
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	public static void serializeResult(A2WSystem ann, A2WDataset ds, File output) throws FileNotFoundException, IOException {
+		List<HashSet<Annotation>> annotations = BenchmarkCache.doA2WAnnotations(ann, ds);
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(output));
+		oos.writeObject(annotations);
+		oos.close();
+	}
+	
+	public static List<HashSet<Annotation>> deserializeResult(File input) throws ClassNotFoundException, FileNotFoundException, IOException {
+		ObjectInputStream oos = new ObjectInputStream(new FileInputStream(input));
+		Object o = oos.readObject();
+		oos.close();
+		return (List<HashSet<Annotation>>) o;
+	}
+	
+	public static void evaluateSerializedResults(A2WDataset ds, File input) throws FileNotFoundException, IOException, ClassNotFoundException {
+		List<HashSet<Annotation>> results = deserializeResult(input);
+		assert results.size() == ds.getSize();
+		
+		Metrics<Tag> metricsTag = new Metrics<>();
+		MetricsResultSet C2WRes = metricsTag.getResult(ProblemReduction.A2WToC2WList(results), ds.getC2WGoldStandardList(), new StrongTagMatch(WikipediaApiInterface.api()));
+		Utils.printMetricsResultSet("C2W", C2WRes, input.getName());
+
+		Metrics<Annotation> metricsAnn = new Metrics<>();
+		MetricsResultSet rsA2W = metricsAnn.getResult(results, ds.getA2WGoldStandardList(), new StrongAnnotationMatch(WikipediaApiInterface.api()));
+		Utils.printMetricsResultSet("A2W-SAM", rsA2W, input.getName());
+	}
+
+	public static void printMetricsResultSet(String exp, MetricsResultSet rs, String annName){
+		System.out
+		.format(Locale.ENGLISH,
+				"%s\t mac-P/R/F1: %.3f\t%.3f\t%.3f TP/FP/FN: %d\t%d\t%d mic-P/R/F1: %.3f\t%.3f\t%.3f\t%s%n", exp,
+				rs.getMacroPrecision(), rs.getMacroRecall(),
+				rs.getMacroF1(), rs.getGlobalTp(),
+				rs.getGlobalFp(), rs.getGlobalFn(),rs.getMicroPrecision(), rs.getMicroRecall(),
+				rs.getMicroF1(), annName);
 	}
 }
