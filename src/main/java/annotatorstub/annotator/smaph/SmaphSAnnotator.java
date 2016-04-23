79b3b31130716f04cf59607420d60a3426bd2fcd
taivop <taivo.pungas@gmail.com>
2016-04-23 17:43:45 +0200

    basic structure of smaph-s annotator

--- src/main/java/annotatorstub/annotator/smaph/SmaphSAnnotator.java
+++ src/main/java/annotatorstub/annotator/smaph/SmaphSAnnotator.java
@@ -1,6 +1,11 @@
 package annotatorstub.annotator.smaph;
 
 import annotatorstub.annotator.FakeAnnotator;
+import annotatorstub.annotator.tagme.TagMeAnnotator;
+import annotatorstub.utils.WATRelatednessComputer;
+import annotatorstub.utils.mention.GreedyMentionIterator;
+import annotatorstub.utils.mention.MentionCandidate;
+import com.sun.tools.javac.util.Pair;
 import it.unipi.di.acube.batframework.data.ScoredAnnotation;
 import it.unipi.di.acube.batframework.utils.AnnotationException;
 import it.unipi.di.acube.batframework.utils.WikipediaApiInterface;
@@ -8,6 +13,7 @@ import it.unipi.di.acube.batframework.utils.WikipediaApiInterface;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Vector;
 
 /**
  * SMAPH-S annotator from the paper "A Piggyback System for Joint Entity Mention Detection and Linking in Web Queries".
@@ -17,36 +23,89 @@ import java.util.Set;
 public class SmaphSAnnotator extends FakeAnnotator {
 
     /**
-     * Returns a set of candidate entities.
+     * Returns a set of candidate entities from the output of TagMeAnnotator.
      *
      * @param query the query as a string
      * @return a set of integers, each corresponding to a Wikipedia ID of a candidate entity.
      */
-    Set<Integer> epsilonSet1(String query) {
-        return null;
-    }
+    public Set<Integer> getMockEpsilonSet1(String query) {
+        HashSet<Integer> candidates = new HashSet<Integer>();
 
-    public HashSet<ScoredAnnotation> solveSa2W(String text) throws AnnotationException {
-        lastTime = System.currentTimeMillis();
-        int start = 0;
-        while (start < text.length() && !Character.isAlphabetic(text.charAt(start)))
-            start++;
-        int end = start;
-        while (end < text.length() && Character.isAlphabetic(text.charAt(end)))
-            end++;
+        TagMeAnnotator ann = new TagMeAnnotator();
+        Set<ScoredAnnotation> scoredAnnotations = ann.solveSa2W(query);
 
-        int wid;
-        try {
-            wid = WikipediaApiInterface.api().getIdByTitle(text.substring(start, end));
-        } catch (IOException e) {
-            throw new AnnotationException(e.getMessage());
+        for(ScoredAnnotation scoredAnnotation : scoredAnnotations) {
+            System.out.println(scoredAnnotation.getConcept());
+            candidates.add(scoredAnnotation.getConcept());
         }
 
-        HashSet<ScoredAnnotation> result = new HashSet<>();
-        if (wid != -1)
-            result.add(new ScoredAnnotation(start, end - start, wid, 0.1f));
-        lastTime = System.currentTimeMillis() - lastTime;
-        return result;
+        return candidates;
+    }
+
+    public Set<Integer> getMockEpsilonSet2(String query) {
+        return new HashSet<Integer>();
+    }
+
+    public Set<Integer> getMockEpsilonSet3(String query) {
+        return new HashSet<Integer>();
+    }
+
+    /**
+     * Returns the SMAPH-S features for given entity. See table 4 in article.
+     *
+     * @param entity the Wikipedia ID of the entity
+     * @return vector of per-entity features
+     */
+    private Vector<Double> getEntityFeatures(Integer entity) {
+        // TODO have a hashmap for this so if we query the same entity again, we won't recalculate stuff.
+
+        return new Vector<>();
+    }
+
+    /**
+     * Returns the SMAPH-S features for given mention-entity pair. See table 5 in article.
+     *
+     * @param mention the mention in query
+     * @param entity the Wikipedia ID of the entity
+     * @return vector of per-pair features
+     */
+    private Vector<Double> getMentionEntityFeatures(MentionCandidate mention, Integer entity) {
+        // TODO
+        return new Vector<>();
+    }
+
+    public HashSet<ScoredAnnotation> solveSa2W(String query) throws AnnotationException {
+
+        // Create set of candidate entities
+        Set<Integer> candidates = new HashSet<Integer>();               // Epsilon_q in the paper
+        candidates.addAll(getMockEpsilonSet1(query));
+        candidates.addAll(getMockEpsilonSet2(query));
+        candidates.addAll(getMockEpsilonSet3(query));
+
+        // Find all potential mentions
+        Set<MentionCandidate> mentions = new HashSet<>();                // Seg(q) in the paper
+        GreedyMentionIterator it = new GreedyMentionIterator(query);
+        while (it.hasNext()) {
+            MentionCandidate mention = it.next();
+            mentions.add(mention);
+        }
+
+        // Calculate features for all possible pairs of <anchor, candidate entity>
+        for(MentionCandidate mention : mentions) {
+            for(Integer entity : candidates) {
+
+                // Get both per-entity features and per-pair features.
+                Vector<Double> entityFeatures = getEntityFeatures(entity);
+                Vector<Double> mentionEntityFeatures = getMentionEntityFeatures(mention, entity);
+
+                Vector<Double> features = new Vector<>();
+                features.addAll(entityFeatures);
+                features.addAll(mentionEntityFeatures);
+            }
+        }
+
+
+        return new HashSet<>();
     }
 
     public String getName() {
