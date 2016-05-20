f3f0768cd70c38a57dbfa07ad300613986314314
taivop <taivo.pungas@gmail.com>
2016-05-20 21:29:28 +0200

    implemented everything necessary now. tested almost nothing; would be surprised if it worked. also ugly but hey, it is Friday at 2130. maybe I should have a beer now

--- src/main/java/annotatorstub/annotator/smaph/SmaphSRemoteSvmPruner.java
+++ src/main/java/annotatorstub/annotator/smaph/SmaphSRemoteSvmPruner.java
@@ -2,15 +2,19 @@ package annotatorstub.annotator.smaph;
 
 import annotatorstub.utils.PythonApiInterface;
 import annotatorstub.utils.mention.SmaphCandidate;
+import annotatorstub.utils.mention.SmaphCandidateScored;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 public class SmaphSRemoteSvmPruner implements SmaphSListPruner {
 
     private PythonApiInterface svmApi;
+    public static final Double PRUNING_THRESHOLD = 0.2;
 
     public SmaphSRemoteSvmPruner(PythonApiInterface svmApi) {
         this.svmApi = svmApi;
@@ -19,15 +23,33 @@ public class SmaphSRemoteSvmPruner implements SmaphSListPruner {
     @Override
     public List<SmaphCandidate> shouldKeep(List<SmaphCandidate> candidates) {
         try {
-            List<Double> scores = new ArrayList<>();
+            PriorityQueue<SmaphCandidateScored> pq = new PriorityQueue<>();
+
             for(SmaphCandidate candidate : candidates) {
-                scores.add(svmApi.binClassifyFlaskProbabilistic(candidate.getFeatures()));
+                Double score = svmApi.binClassifyFlaskProbabilistic(candidate.getFeatures());
+                pq.add(new SmaphCandidateScored(candidate, score));
             }
 
-            // TODO(Taivo) this is where the magic should happen:
-            // pick highest-scored non-overlapping candidates until a threshold is reached
-            
-            return candidates;
+            List<SmaphCandidateScored> acceptedCandidates = new ArrayList<>();
+            while(!pq.isEmpty()) {
+                SmaphCandidateScored scoredCandidate = pq.poll();
+
+                // If the current candidate is below threshold, we know all the subsequent ones will be, too.
+                if(scoredCandidate.getScore() < PRUNING_THRESHOLD) {
+                    break;
+                }
+
+                // Accept only candidates that don't overlap with previously chosen ones.
+                if(!scoredCandidate.overlapsAny(acceptedCandidates)) {
+                    acceptedCandidates.add(scoredCandidate);
+                }
+            }
+
+            List<SmaphCandidate> acceptedCandidatesWithoutScores = acceptedCandidates.stream()
+                    .map(SmaphCandidateScored::getSmaphCandidate)
+                    .collect(Collectors.toList());
+
+            return acceptedCandidatesWithoutScores;
         }
         catch(IOException ex) {
             throw new RuntimeException("Could not access Python API.", ex);
---
+++ src/main/java/annotatorstub/utils/mention/SmaphCandidateScored.java
@@ -0,0 +1,54 @@
+package annotatorstub.utils.mention;
+
+
+import it.unipi.di.acube.batframework.data.Mention;
+
+import java.util.Collection;
+
+/**
+ * Simple wrapper class to attach scores to SmaphCandidates.
+ */
+public class SmaphCandidateScored implements Comparable<SmaphCandidateScored> {
+
+    private SmaphCandidate smaphCandidate;
+    private Double score;
+
+    public SmaphCandidateScored(SmaphCandidate smaphCandidate, Double score) {
+        this.smaphCandidate = smaphCandidate;
+        this.score = score;
+    }
+
+    public SmaphCandidate getSmaphCandidate() {
+        return smaphCandidate;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public int compareTo(SmaphCandidateScored that) {
+        return Double.compare(this.score, that.score);
+    }
+
+    public boolean overlaps(SmaphCandidateScored that) {
+        MentionCandidate thisMentionCandidate = this.smaphCandidate.getMentionCandidate();
+        MentionCandidate thatMentionCandidate = that.smaphCandidate.getMentionCandidate();
+
+        Mention thisMention =
+                new Mention(thisMentionCandidate.getQueryStartPosition(),thisMentionCandidate.getLength());
+        Mention thatMention =
+                new Mention(thatMentionCandidate.getQueryStartPosition(), thatMentionCandidate.getLength());
+
+        return thisMention.overlaps(thatMention);
+    }
+
+    public boolean overlapsAny(Collection<SmaphCandidateScored> collection) {
+        for(SmaphCandidateScored that : collection) {
+            if(this.overlaps(that))
+                return true;
+        }
+        return false;
+    }
+
+
+}
