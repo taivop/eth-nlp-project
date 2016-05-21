53e3f82b009c05ad5bd5864a76fbf593b8e4e25f
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2016-05-21 16:04:05 +0200

    Another experimental query.

--- src/test/java/annotatorstub/utils/modification/TestQueryProcessing.java
+++ src/test/java/annotatorstub/utils/modification/TestQueryProcessing.java
@@ -7,12 +7,14 @@ import annotatorstub.utils.WATRelatednessComputer;
 import annotatorstub.utils.bing.BingSearchAPI;
 
 public class TestQueryProcessing {
-	public static void main (String[] args) throws FileNotFoundException, ClassNotFoundException, IOException {
+	public static void main (String[] args) throws Exception {
 		WATRelatednessComputer.setCache("relatedness.cache");
 		BingSearchAPI.KEY = "+MkADwXpOGeryP7sNqlkbtUeZYhs8mUeUBsNq++Yk1U";
 		QueryProcessing instance = QueryProcessing.getInstance();
 		
-		String query = "north20carolina20in20aolmsnearthlinknetscapesympat";
+//		String query = "north20carolina20in20aolmsnearthlinknetscapesympat";
+		String query = "how29isbabbyformed";
+
 		String newQuery = instance.alterQueryForBingSearch(query);
 		
 		System.out.println("old query: "+query);
