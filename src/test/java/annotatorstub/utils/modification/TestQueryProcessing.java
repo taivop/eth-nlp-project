dd788313952efab6f8482582b54f2d730e2688d4
Bernhard Kratzwald <bernhard2202@gmail.com>
2016-05-21 17:26:18 +0200

    debuged query modifiaction better testing

--- src/test/java/annotatorstub/utils/modification/TestQueryProcessing.java
+++ src/test/java/annotatorstub/utils/modification/TestQueryProcessing.java
@@ -2,21 +2,39 @@ package annotatorstub.utils.modification;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import annotatorstub.utils.WATRelatednessComputer;
 import annotatorstub.utils.bing.BingSearchAPI;
+import annotatorstub.utils.mention.GreedyMentionIterator;
+import annotatorstub.utils.mention.MentionCandidate;
+import it.unipi.di.acube.batframework.datasetPlugins.DatasetBuilder;
+import it.unipi.di.acube.batframework.datasetPlugins.GERDAQDataset;
 
 public class TestQueryProcessing {
+	
+	
+	private static List<String> getModifications(GERDAQDataset dataset, QueryProcessing instance) {
+		List<String> changes  = new ArrayList<>();
+		for (String query : dataset.getTextInstanceList()) {
+			String altered = instance.alterQueryForBingSearch(query);
+			if (query.split(" ").length==1 && !query.equals(altered)) {
+				changes.add(query+" -> "+altered);
+			}
+		}
+		return changes;
+	}
+	
 	public static void main (String[] args) throws FileNotFoundException, ClassNotFoundException, IOException {
 		WATRelatednessComputer.setCache("relatedness.cache");
 		BingSearchAPI.KEY = "+MkADwXpOGeryP7sNqlkbtUeZYhs8mUeUBsNq++Yk1U";
-		QueryProcessing instance = QueryProcessing.getInstance();
+		QueryProcessing instance = QueryProcessing.getInstance();		
+
 		
-		String query = "north20carolina20in20aolmsnearthlinknetscapesympat";
-		String newQuery = instance.alterQueryForBingSearch(query);
-		
-		System.out.println("old query: "+query);
-		System.out.println("new query: "+newQuery);
-		
+		List<String> modifications = getModifications(DatasetBuilder.getGerdaqTrainA(), instance);
+		modifications.addAll(getModifications(DatasetBuilder.getGerdaqTrainB(), instance));
+		modifications.addAll(getModifications(DatasetBuilder.getGerdaqDevel(), instance));
+		modifications.addAll(getModifications(DatasetBuilder.getGerdaqTest(), instance));
 	}
 }
