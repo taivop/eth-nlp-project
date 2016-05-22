7f6e7659de8d59202c4479569aa52369b1cac714
AndreiBarsan <andrei.ioan.barsan@gmail.com>
2016-05-22 13:07:38 +0200

    More numbers.

--- src/main/java/annotatorstub/main/BenchmarkMain.java
+++ src/main/java/annotatorstub/main/BenchmarkMain.java
@@ -26,10 +26,10 @@ import java.util.Optional;
 public class BenchmarkMain {
     public static void main(String[] args) throws Exception {
         WikipediaApiInterface wikiApi = WikipediaApiInterface.api();
+
+        // We are evaluating only on the GERDAQ-Test dataset.
         A2WDataset ds = DatasetBuilder.getGerdaqTest();
-//		FakeAnnotator ann = new FakeAnnotator();
-//		BaselineAnnotator ann = new BaselineAnnotator();
-//		WATAnnotator ann = new WATAnnotator("wikisense.mkapp.it", 80, "salsa-auth");
+
         /*
          This part is using the Smaph-1/S annotation system. To get it working, there are a few
          steps which need to be done.
@@ -49,16 +49,19 @@ public class BenchmarkMain {
             6) (Optional) Move these instructions to a more appropriate place, if applicable.
          */
 
-        try (PythonApiInterface svmApi = new PythonApiInterface(5001)) {
+        try (PythonApiInterface svmApi = new PythonApiInterface(5103)) {
             // Use a separate cache when running the benchmark as opposed to when doing the data
             // generation, since this lets us keep the benchmark-only cache small. The data gen
             // one, especially when also using the Yahoo! data, ends up blowing up to several Gb,
             // and takes around a minute to load.
             WATRequestCache watRequestCache = new WATRequestCache(
                 "watapi.benchmark.cache",
-                "Small WAT API cache (benchmark only)", 500);
+                "Small WAT API cache (benchmark only)",
+                500);
 //            svmApi.startPythonServer("models/m-no-yahoo-svc-c-0.1000-probabilistic.pkl");
-            svmApi.startPythonServer("models/m-no-yahoo-svc-c-0.0005-probabilistic.pkl");
+//            String modelPickle = "models/m-no-yahoo-lr-c-0.001.pkl";
+            String modelPickle = "models/m-no-yahoo-svc-c-1.0000-probabilistic.pkl";
+            svmApi.startPythonServer(modelPickle);
             SmaphSAnnotator ann = new SmaphSAnnotator(
 //                new SmaphSIndividualPruner(new Smaph1RemoteSvmPruner(svmApi)),
                 new SmaphSRemoteSvmPruner(svmApi),
@@ -101,6 +104,7 @@ public class BenchmarkMain {
 
             // TODO-LOW(andrei): Use more dependency injection instead of this.
             ((HelperWATAnnotator) ann.getAuxiliaryAnnotator()).getRequestCache().flush();
+            System.out.println("Was using following model: " + modelPickle);
         }
     }
 
